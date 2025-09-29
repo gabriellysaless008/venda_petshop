3885f3f121d0bf5335a6b32216b113f71f56557a
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-09-29 10:55:30 -0300

    sexto commit

--- form.pet/bootstrap-5.3.6-dist/bootstrap-5.3.6-dist/js/form.js
+++ form.pet/bootstrap-5.3.6-dist/bootstrap-5.3.6-dist/js/form.js
@@ -4,6 +4,13 @@ function entrar(){
     const senha = document.getElementById("senha").value;
 }
 
-function validarUsuario(){
+function criarContaUsuario(){
+    const nome = document.getElementById("nome").value;
+
+    const email = document.getElementById("email").value;
+
+    const senha = document.getElementById("senha").value;
+
+    const telefone = document.getElementById("telefone").value;
     
 }
