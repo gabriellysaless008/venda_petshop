8fc619210a6ee4045e7af8dc1f7fbffce41e0809
gabriellysaless008 <gabrielly.sales.silva@escola.pr.gov.br>
2025-10-03 11:19:22 -0300

    nono commit

--- form.pet/bootstrap-5.3.6-dist/bootstrap-5.3.6-dist/js/form.js
+++ form.pet/bootstrap-5.3.6-dist/bootstrap-5.3.6-dist/js/form.js
@@ -8,13 +8,13 @@ function criarContaUsuario(){
     const nome = document.getElementById("nome").value;
 
     const email = document.getElementById("email").value;
-<<<<<<< HEAD
+
     
     const senha = document.getElementById("senha").value;
 
     const telefone = document.getElementById("telefone").value;
 
-=======
+
 
     const senha = document.getElementById("senha").value;
 
@@ -24,6 +24,4 @@ function criarContaUsuario(){
 
 
 function teste(){
->>>>>>> 98d5c0c97aba6a60c915b7da26d342e8cf7df8fa
-    
 }
